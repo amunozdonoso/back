8a42fe2bf9010b8dfe730d6e5bd94cd63c94338b
amunozdonoso <118765359+amunozdonoso@users.noreply.github.com>
2022-11-21 16:28:39 -0300

    repo completo

---
+++ spring-boot-backend-apirest/spring-boot-backend-azurian/src/main/java/com/azurian/springboot/backend/prueba/SpringBootBackendAzurianApplication.java
@@ -0,0 +1,13 @@
+package com.azurian.springboot.backend.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendAzurianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendAzurianApplication.class, args);
+	}
+
+}
---
+++ spring-boot-backend-apirest/spring-boot-backend-azurian/src/test/java/com/azurian/springboot/backend/prueba/SpringBootBackendAzurianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azurian.springboot.backend.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendAzurianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
